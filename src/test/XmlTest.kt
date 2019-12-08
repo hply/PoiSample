8a200d45135b89e7e00e3ea44eab84456dd13352
wjd <ijiedong@aliyun.com>
2019-12-09 01:29:00 +0800

    删除测试文件,新增测试代码

---
+++ src/test/XmlTest.kt
@@ -0,0 +1,73 @@
+package test
+
+import org.dom4j.Document
+import org.dom4j.DocumentHelper
+import org.dom4j.DocumentType
+import org.dom4j.Element
+import org.dom4j.io.OutputFormat
+import org.dom4j.io.SAXReader
+import org.dom4j.io.XMLWriter
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
+
+const val out_xml_demo_path = "src/strings.xml"
+const val in_xml_demo_path = "src/strings_backup.xml"
+
+fun main() {
+    val dom = load(in_xml_demo_path)!!
+    val resourcesElement = dom.rootElement
+    val elements = resourcesElement.elements("string")
+    val list = arrayListOf<Pair<String, String>>()
+    elements.forEach {
+        val lanCode: String = it.attributeValue("name")
+        val lanValue: String = it.textTrim
+        list.add(Pair(lanCode, lanValue))
+    }
+    val newList = arrayListOf<Pair<String, String>>()
+
+    var tempLength = 10
+    while (true) {
+        if (newList.size in 1..100) {
+            break
+        }
+        newList.clear()
+        list.filter {
+            it.second.length < tempLength
+        }.forEach {
+            newList.add(it)
+        }
+        tempLength--
+    }
+    elements.forEach {
+        resourcesElement.remove(it)
+    }
+    newList.forEach {
+        val stringEle: Element = resourcesElement.addElement("string")
+        stringEle.addAttribute("name", it.first)
+        stringEle.text = it.second
+    }
+    //命名空间
+//    resourcesElement.addNamespace("android","http://schemas.android.com/apk/res/android")
+//    resourcesElement.addNamespace("app","http://schemas.android.com/apk/res-auto")
+//    resourcesElement.addNamespace("tools","http://schemas.android.com/tools")
+
+    val format: OutputFormat = OutputFormat.createPrettyPrint()
+    format.encoding = "utf-8"
+    val streamWriter = OutputStreamWriter(FileOutputStream(out_xml_demo_path), "UTF-8")
+    val writer = XMLWriter(streamWriter, format)
+    writer.write(dom)
+    writer.flush()
+    writer.close()
+}
+
+fun load(fileName: String): Document? {
+    var document: Document? = null
+    try {
+        val saxReader = SAXReader()
+        document = saxReader.read(File(fileName)) // 读取XML文件,获得document对象
+    } catch (ex: Exception) {
+        ex.printStackTrace()
+    }
+    return document
+}
